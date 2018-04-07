619a2d3e2f235bc8de17ec2c0c607c4ec5a00c9c
Maneesha Wijesinghe <mkwmanisha@gmail.com>
2018-04-07 11:39:31 +0530

    open menu animations added

--- app/src/main/java/com/example/user/mrbin/menu/Home.java
+++ app/src/main/java/com/example/user/mrbin/menu/Home.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import com.example.user.mrbin.R;
 
 public class Home extends AppCompatActivity {
-
+   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.Home);
+
     }
 }
